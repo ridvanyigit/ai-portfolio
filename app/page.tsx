ad625e0c604f653bc653bb9d391edd1a5fa4fb67
ridvanyigit <ridvanyigit@gmx.net>
2025-10-17 10:43:53 +0200

    fix: Combine floating window with new UI correctly

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import AboutSection from "./(sections)/about/AboutSection";
 import ServicesSection from "./(sections)/services/ServicesSection";
 import ProjectsSection from "./(sections)/projects/ProjectsSection";
 import ContactSection from "./(sections)/contact/ContactSection";
-import ChatbotSection from "./(sections)/chatbot/ChatbotSection";
 
 export default function Home() {
   return (
