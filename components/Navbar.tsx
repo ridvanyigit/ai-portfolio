8af68ca3d4f4ac8ebad2b075b1c0e2cf319200f4
ridvanyigit <ridvanyigit@gmx.net>
2025-10-17 18:22:14 +0200

    modified globals.css & Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,9 +7,9 @@ export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full backdrop-blur bg-background/70 border-b border-border z-50">
       <div className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
-        <Link href="/" className="font-signature font-nabla-effect text-4xl">
-          Rıdvan Yiğit
-        </Link>
+      <Link href="/" className="bg-gradient-to-r from-indigo-500 to-purple-600 bg-clip-text text-transparent font-bold text-3xl">
+        Rıdvan Yiğit
+      </Link>
         <div className="flex items-center gap-4">
           <div className="space-x-4 text-sm font-medium hidden md:block">
             <Link href="/#about" className="hover:text-indigo-500">About</Link>
