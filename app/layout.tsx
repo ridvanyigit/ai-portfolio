ed0cd12b6bca635d8c4fe40c361348072fc1b533
ridvanyigit <ridvanyigit@gmx.net>
2026-01-22 11:00:14 +0100

    VoiceAgent added

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,43 +20,22 @@ export const metadata: Metadata = {
   },
 };
 
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Nabla&display=swap"
-          rel="stylesheet"
-        />
-
-        {/* GA4 Tag */}
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-VR1KYMRE4P"
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-VR1KYMRE4P');
-          `}
-        </Script>
-      </head>
       <body>
-        {/* Fixed top navbar */}
         <Navbar />
-
-        {/* Page content */}
         <div className="pt-16">{children}</div>
 
-        {/* Fixed chatbot icon + iframe at bottom right */}
-        <FloatingChatbot />
+        {/* MEVCUT CHATBOT'U YORUM SATIRI YAPARAK DEVRE DIŞI BIRAKIYORUZ */}
+        {/* <FloatingChatbot /> */}
 
-        {/* Footer at the bottom */}
         <Footer />
+
+        {/* YENİ ELEVENLABS SESLİ CHATBOT'U BURAYA EKLİYORUZ */}
+        <elevenlabs-convai agent-id="agent_6301kfjerfxnfxt9mmeaam1mggrs"></elevenlabs-convai>
+        <script src="https://unpkg.com/@elevenlabs/convai-widget-embed" async type="text/javascript"></script>
       </body>
     </html>
   );
